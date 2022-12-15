f92a7693b7630423cb6397058a99c92310e95f81
Guilherme Borlot <guilherme.borlotoliveira@gubee.com.br>
2022-12-15 18:05:17 -0300

    primeiro commit adicionando esqueleto spring boot

---
+++ proinfo-backend/src/test/java/br/com/proinfo/ProinfoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.proinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProinfoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
